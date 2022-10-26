e81b7111e8290d25992197d753de474122001efd
Dinh_than <dinhthanh872@gmail.com>
2022-10-27 00:09:46 +0700

    server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// connect mongoose
+mongoose.connect(process.env.MONGODB_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+}).then(() => { console.log('connect mongodb successfully') })
+	.catch((err) => { console.log(err.message) })
+
+const server = app.listen(process.env.PORT, () =>
+{
+	console.log('listening on port ' + process.env.PORT)
+})
+
+
